9c1d566d761bd82766fde0534bcf95121acc153f
David Thompson <thompd10@mcmaster.ca>
2019-06-18 17:05:01 -0400

    More work on TaskList interface

--- headers/TaskList.h
+++ headers/TaskList.h
@@ -1,4 +1,9 @@
-
+/**
+ * \file TaskList.h
+ * \author David Thompson
+ * \brief Defines the TaskList class
+ * \date 2019-06-18
+ */
 
 #ifndef TASK_LIST_H
 #define TASK_LIST_H
@@ -32,6 +37,30 @@ public:
      * \param expiry_policy The policy to determine if an item is expired
      */
     void remove_expired(ExpiryPolicy expiry_policy);
+
+    /**
+     * \brief Add a new task to this TaskList
+     * 
+     * \param task The Task to add
+     */
+    void add_new(Task task);
+
+    // TODO: Make an iterator getter
+
+    /**
+     * \brief Convert this TaskList to a computer readable string
+     * 
+     * \returns The TaskList represented as a computer readable string
+     */
+    std::string to_string();
+
+    /**
+     * \brief Make a TaskList from a computer readable TaskList string
+     * 
+     * \returns The TaskList that the string represents
+     * \throws invalid_argument if the string is not in the expected format
+     */
+    static TaskList from_string(std::string string_rep);
 
 };
 
