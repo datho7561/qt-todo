bf7f90f3348d7acb8ff475967b558c97c3aaa454
David Thompson <thompd10@mcmaster.ca>
2019-09-01 10:41:35 -0400

    Deadline is now displayed
    Updated the tasks.json for my new setup using qtchooser and qmake defaulting to Qt5
    Adding Snapcraft file, but it isn't ready yet

--- src/TaskWidget.cpp
+++ src/TaskWidget.cpp
@@ -30,20 +30,39 @@ TaskWidget::TaskWidget(Task * task, QWidget * parent):
 void TaskWidget::update_text() {
 
     std::string new_text = "";
+    std::string new_date_text = "";
+
     if (task->is_complete()) {
         new_text += "<s><span style=\"color: gray\">";
+        new_date_text += "<s><span style=\"color: gray\">";
     }
     if (task->is_overdue()) {
         new_text += "<span style=\"color: red\">";
+        new_date_text += "<span style=\"color: red\">";
     }
+    
     new_text += task->get_name();
+
+    if (task->get_deadline() == Date()) {
+        new_date_text += tr("Today").toStdString();
+    } else if (task->get_deadline() == Date().yesterday()) {
+        new_date_text += tr("Yesterday").toStdString();
+    } else if (task->get_deadline().yesterday() == Date()) {
+        new_date_text += tr("Tomorrow").toStdString();
+    } else {
+        new_date_text += task->get_deadline().to_string();
+    }
+
     if (task->is_overdue()) {
         new_text += "</span>";
+        new_date_text += "</span>";
     }
     if (task->is_complete()) {
         new_text += "</span></s>";
+        new_date_text += "</span></s>";
     }
     task_label->setText(QString::fromStdString(new_text));
+    date_label->setText(QString::fromStdString(new_date_text));
     this->repaint();
 }
 
